d4ead86bc37aaf507099466533d9157d622bf207
Patrick Wong <wongpatrick280@gmail.com>
2018-05-04 06:01:47 +0000

    added creation

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs")
 
 var campgrounds = [
@@ -16,6 +18,18 @@ app.get("/", function(req,res){
 
 app.get("/campgrounds", function(req,res){
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req,res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req,res){
+   res.render("new.ejs"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
