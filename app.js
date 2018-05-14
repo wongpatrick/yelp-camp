9d1e08687a933f43526407b80ef33e0386d592ef
Patrick Wong <wongpatrick280@gmail.com>
2018-05-14 05:37:18 +0000

    Finished section 31

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var express = require("express"),
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs")
 
 
@@ -25,7 +26,7 @@ app.get("/campgrounds", function(req,res){
        if (err) {
            console.log(err);
        } else {
-           res.render("index", {campgrounds: allCampgrounds});
+           res.render("campgrounds/index", {campgrounds: allCampgrounds});
        }
     });
     
@@ -46,7 +47,7 @@ app.post("/campgrounds", function(req,res){
 });
 
 app.get("/campgrounds/new", function(req,res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new"); 
 });
 
 // SHOW - shows more info about one campground
@@ -56,11 +57,44 @@ app.get("/campgrounds/:id", function(req,res){
            console.log(err);
        } else {
            console.log(foundCampground);
-        res.render("show", {campground: foundCampground});
+        res.render("campgrounds/show", {campground: foundCampground});
        }
     });
    
 });
+
+// ========
+// COMMENTS Routes
+// ========
+
+app.get("/campgrounds/:id/comments/new", function(req,res){
+    Campground.findById(req.params.id, function(err,campground){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    Campground.findById(req.params.id, function(err,campground){
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+ campground._id);
+                }
+            })
+        }
+    });
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp app has started");
