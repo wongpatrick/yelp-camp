60b699aad9bc8a698ebafa1c4b4ff489653d4662
Patrick Wong <wongpatrick280@gmail.com>
2018-05-23 04:59:50 +0000

    Added price and landing page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,13 +18,14 @@ router.get("/", function(req,res){
 // Create POST Route
 router.post("/", middleware.isLoggedIn, function(req,res){
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var description = req.body.description;
     var author = {
         id: req.user._id,
         username:req.user.username
     };
-    var newCampground = {name: name, image: image, description: description, author: author};
+    var newCampground = {name: name, price: price, image: image, description: description, author: author};
     Campground.create(newCampground, function(err, newlyCreated){
         if (err) {
             console.log(err);
