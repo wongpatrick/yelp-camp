0273d2ddcf41fee0350411faf9075cd72607009f
Patrick Wong <wongpatrick280@gmail.com>
2018-05-28 02:30:11 +0000

    Added createdAt and fixed pricing for new

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,6 +32,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
+  var price = req.body.price;
   var author = {
       id: req.user._id,
       username: req.user.username
@@ -44,7 +45,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var lat = data[0].latitude;
     var lng = data[0].longitude;
     var location = data[0].formattedAddress;
-    var newCampground = {name: name, image: image, description: desc, author:author, location: location, lat: lat, lng: lng};
+    var newCampground = {name: name, image: image, description: desc, price: price, author:author, location: location, lat: lat, lng: lng};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
